c1bdbe458865859794f114f9af7b1d28ebf9eeba
Khandaker Samin Yeasar <khandakersaminyeasar@gmail.com>
2023-09-24 19:30:18 +0600

    react-router and tailwind setup and favicon or navber added

---
+++ src/Header/NavLogo.jsx
@@ -0,0 +1,10 @@
+
+const NavLogo = () => {
+    return (
+        <div className="w-[172px] h-[50px]">
+            <img src="./../../public/Img/Logo.png" alt="Donation Campaign" />
+        </div>
+    );
+};
+
+export default NavLogo;
---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,12 @@
+
+
+const ErrorPage = () => {
+    return (
+        <div className="h-screen flex justify-center text-center items-center text-4xl font-bold">
+            Opps !! <br /> <br />
+            Some Error Here ....
+        </div>
+    );
+};
+
+export default ErrorPage;
